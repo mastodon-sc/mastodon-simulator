d740539e6936c56f6120d2aef132f97a098de383
Vladimír Ulman <ulman@fi.muni.cz>
2025-08-02 22:01:54 +0200

    FIX: renamed 'Generate_regular_lineage' -> 'Generate_regular_cubes_with_lineage', and...
    ...updated/fixed the 'Open all scripts in editor' to also open the download script

--- Fiji/scripts/Open_all_scripts_in_editor.py
+++ Fiji/scripts/Open_all_scripts_in_editor.py
@@ -1,6 +1,7 @@
 from ij import IJ
 IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Simulate_lineage_new_project.py");
 IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Simulate_lineage_into_project.py");
-IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Generate_regular_lineage.py");
+IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Generate_regular_cubes_with_lineage.py");
 IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Generate_regular_cubes.py");
+IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Download_reference_mastodon_projects.py");
 
