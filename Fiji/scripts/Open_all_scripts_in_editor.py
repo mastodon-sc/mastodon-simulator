de13f58214a1c33997d30503dc0bd279d193fd27
Vladimír Ulman <ulman@fi.muni.cz>
2025-08-04 12:16:04 +0200

    FIX: renamed some of the scripts, "open_all" scripts indeed opens all

--- Fiji/scripts/Open_all_scripts_in_editor.py
+++ Fiji/scripts/Open_all_scripts_in_editor.py
@@ -1,7 +1,7 @@
 from ij import IJ
-IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Simulate_lineage_new_project.py");
-IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Simulate_lineage_into_project.py");
-IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Generate_regular_cubes_with_lineage.py");
+IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Run_simulation_in_new_project.py");
+IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Run_simulation_in_existing_project.py");
 IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Generate_regular_cubes.py");
+IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Generate_regular_cubes_with_lineage.py");
 IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Download_reference_mastodon_projects.py");
-
+IJ.run("Edit...", "open=scripts/Plugins/Tracking/Mastodon/Simulator/Open_all_scripts_in_editor.py");
